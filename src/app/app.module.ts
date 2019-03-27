b0c07d5218e273b15e51e55978b48566c937c9bb
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-03-27 16:27:25 +0200

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkerComponent } from './worker/worker.component';
+
+import { WorkersService } from './workers.service';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { MatButtonModule, MatMenuModule, MatDatepickerModule, MatNativeDateModule  } from '@angular/material';
+import { MatInputModule, MatTooltipModule, MatCardModule, MatIconModule, MatFormFieldModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [ WorkersService, HttpClientModule ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
